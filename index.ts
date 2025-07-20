164d9c740bc236883cf0377edfb51c05aedf01ac
Mike <mickael.djegherif@outlook.fr>
2025-07-20 19:54:19 +0200

    update index pour deploiement

---
+++ index.ts
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const db = require('./src/db/connection.ts');
+
+
+db.connect()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err: unknown) => {
+    console.error('Database connection error:', err);
+  });
--- src/app.ts
+++ src/app.ts
@@ -2,34 +2,33 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import sequelize from './db/sequelize';
 
 import './models'; // Initialiser les modèles
 
 
 const app = express();
-const port = 3001;
 
 const chatRoutes = require('./routes/chatRoutes');
 
 app.use(express.json());
-
-// Monter les routes
 app.use('/api/chat', chatRoutes);
 
-app.get('/', async (req, res) => {
-    try {
-      await sequelize.authenticate();
-      res.json({
-        message: 'Connexion réussie à la base de données !',
-        time: new Date().toISOString(),
-      });
-    } catch (error) {
-      console.error('Erreur de connexion à la base de données :', error);
-      res.status(500).json({ error: 'Connexion échouée.' });
-    }
-  });
+// app.get('/', async (req, res) => {
+//     try {
+//       await sequelize.authenticate();
+//       res.json({
+//         message: 'Connexion réussie à la base de données !',
+//         time: new Date().toISOString(),
+//       });
+//     } catch (error) {
+//       console.error('Erreur de connexion à la base de données :', error);
+//       res.status(500).json({ error: 'Connexion échouée.' });
+//     }
+//   });
 
 // app.listen(port, () => {
 //   console.log(`Chat service listening at http://localhost:${port}`);
 // });
+
+
+module.exports = app;
