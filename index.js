a69cb542ac107c702494676e380c7be50030e0ea
Mike <mickael.djegherif@outlook.fr>
2025-07-20 23:12:54 +0200

    Fix 2

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ const app = require('./src/app');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const db = require('./src/db/connection');
-
-
-db.connect()
-  .then(() => {
-    console.log('Connected to the database');
-  })
-  .catch(err => {
-    console.error('Database connection error:', err);
-  });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
