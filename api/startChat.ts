d119ae18ad6d1fb6b5a425c852daf1c740578deb
Mike <mickael.djegherif@outlook.fr>
2025-07-20 20:11:42 +0200

    test sans startChat

--- api/startChat.ts
+++ api/startChat.ts
@@ -1,7 +1,1 @@
-import app from '../dist/app';
-import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  // Lancer Express en tant que handler Serverless
-  app(req, res);
-}
