e259553b5725b1e8f9399be032e2a76760cad412
Mike <mickael.djegherif@outlook.fr>
2025-07-20 19:28:14 +0200

    Ajout de la suppression d'un msg, d'une conversation et ajout d'une conversation

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -16,6 +16,19 @@ export const getListeChat = async (req: Request, res: Response) => {
   }
 };
 
+export const postAddListeChat = async (req: Request, res: Response) => {
+  const { user_id, company_id } = req.body;
+
+  try {
+    const messages = await ListChat.create({ user_id, company_id });
+
+    res.json(messages);
+  } catch (error) {
+    console.error('Erreur lors de l\'ajout à la liste de message :', error);
+    res.status(500).json({ error: 'Erreur interne du serveur' });
+  }
+};
+
 export const getMessages = async (req: Request, res: Response) => {
   const { chat_id } = req.params;
 
@@ -35,18 +48,56 @@ export const sendMessage = async (req: Request, res: Response) => {
   const { chat_id, sender_id, content } = req.body;
 
   try {
-    const message = await Chat.create({ chat_id, sender_id, content });
+    const messages = await Chat.create({ chat_id, sender_id, content });
 
-    res.json(message);
+    res.json(messages);
   } catch (error) {
     console.error('Erreur lors de l\'envoi du message :', error);
     res.status(500).json({ error: 'Erreur interne du serveur' });
   }
 };
 
+export const delItemListMessage = async (req: Request, res: Response) => {
+  const  id  = req.params.chat_id;
+
+  try {
+    const deletedCount = await ListChat.destroy({
+      where: { id },
+    });
+
+    if (deletedCount === 0) {
+      return res.status(404).json({ error: 'Aucun item trouvé à supprimer' });
+    }
+
+    res.json({ message: 'Item supprimé avec succès' });
+  } catch (error) {
+    console.error('Erreur lors de la suppression du chat :', error);
+    res.status(500).json({ error: 'Erreur interne du serveur' });
+  }
+};
+
+export const delMessage = async (req: Request, res: Response) => {
+  const  id  = req.params.message_id;
+
+  try {
+    const deletedCount = await Chat.destroy({
+      where: { id },
+    });
+
+    if (deletedCount === 0) {
+      return res.status(404).json({ error: 'Le message à supprimer, n\'est pas trouvé' });
+    }
+
+    res.json({ message: 'Message supprimé avec succès' });
+  } catch (error) {
+    console.error('Erreur lors de la suppression du message :', error);
+    res.status(500).json({ error: 'Erreur interne du serveur' });
+  }
+};
 
 
-// A prevoir suppression de la liste de chat
-// A prevoir suppression de la d'un chat ??
+
+// A prevoir suppression d'un item de la liste de chat -> supprimer tout les chats?
+// A prevoir suppression d'un chat
 
 
--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -5,11 +5,12 @@ const chatController = require('../controllers/chatController');
 
 
 router.get('/:user_id/listes-messages', chatController.getListeChat);
+router.post('/add-listes-messages', chatController.postAddListeChat);
 router.get('/get-messages/:chat_id', chatController.getMessages);
 router.post('/send-message', chatController.sendMessage);
-// router.get('/reponse-message', chatController.getReponseMessage);
 
-  
+router.delete('/del-item-list-message/:chat_id', chatController.delItemListMessage);
+router.delete('/del-message/:message_id', chatController.delMessage);
 
 
 module.exports = router;
