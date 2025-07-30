be76bac3b8657a133e89b1103da1e72c551bba46
Mike <mickael.djegherif@outlook.fr>
2025-07-30 15:51:45 +0200

    add sequelize

---
+++ db/sequelize.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  logging: true, // ou true si tu veux voir les requêtes
+});
+
+module.exports = sequelize;
