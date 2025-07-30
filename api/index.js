a39d78d795297f633467fe49fafcb9788834471d
Mike <mickael.djegherif@outlook.fr>
2025-07-30 16:22:56 +0200

    fix: index

--- api/index.js
+++ api/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const cors = require('cors');
 const chatRoutes = require('../routes/chatRoutes');
 
 const app = express();
 
-const cors = require('cors');
 app.use(cors());
-
-// Middleware pour lire le JSON
 app.use(express.json());
 
-// Route principale pour affichage "page d'acceuil"
 app.get('/', (req, res) => {
-  res.send('Hello depuis Express sur Vercel !');
+  res.send('Hello depuis Express sans Sequelize sur Vercel !');
 });
-// Préfixe les routes avec /api/chat
+
 app.use('/api/chat', chatRoutes);
 
-// Export pour Vercel
-module.exports.handler = serverless(app);
+// 🔴 La ligne incorrecte qui provoque l’erreur :
+// module.exports.handler = serverless(app);
+
+// ✅ La bonne ligne :
+module.exports = serverless(app);
